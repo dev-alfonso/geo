ae6b1b60f91c7fb4a24485ad20d32b81c848ab8c
Vinícius Alfonso <vinnialfonso@hotmail.com>
2021-02-25 13:49:17 -0300

    Check internet on sync, format date and custom messages

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,13 +1,14 @@
 import React, { Component, useState } from "react";
-import { Button, Modal, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Button, Modal, StyleSheet, Text, TextInput, ToastAndroid, TouchableOpacity, View } from "react-native";
 
 import MapView, { Marker, Callout } from "react-native-maps"
 import Geolocation from "react-native-geolocation-service";
 import { useFormik } from "formik";
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import axios from 'axios'
+import axios from 'axios';
+import NetInfo from "@react-native-community/netinfo";
 
-import FormNote from '../components/FormNote'
+import FormNote from '../components/FormNote';
 
 export default class App extends Component {
 
@@ -51,17 +52,22 @@ export default class App extends Component {
         (position) => {
           let marker, latitude, longitude;
           
-          let test = 0.001 * this.state.markers.length;
+          //let test = 0.001 * this.state.markers.length;
 
-          latitude = position.coords.latitude + test;
-          longitude = position.coords.longitude + test;
+          latitude = position.coords.latitude;// + test;
+          longitude = position.coords.longitude;// + test;
 
           let datetime = new Date();
-          let dateString = datetime.getDate() + "/" + (datetime.getMonth()+1) + "/" +  datetime.getFullYear() + " " + datetime.getHours() + ":" + datetime.getMinutes() + ":" + datetime.getSeconds();
+          let dateString = ('0' + datetime.getDate()).slice(-2) + '/'
+                           + ('0' + (datetime.getMonth()+1)).slice(-2) + '/'
+                           + datetime.getFullYear() + ' '
+                           + ('0' + (datetime.getHours()+1)).slice(-2) + ':'
+                           + ('0' + (datetime.getMinutes()+1)).slice(-2) + ':'
+                           + datetime.getSeconds();
 
           marker = {
-            title: "Anotação",
-            description: value.note + "\n\n" + dateString,
+            title: value.note,
+            date: dateString,
             latlng: {
               latitude: latitude,
               longitude: longitude
@@ -83,32 +89,60 @@ export default class App extends Component {
     );
   }
 
-  syncMarkers(){
-    this.setState({ syncing: true });
-    let markers = this.state.markers.filter((m) => !m.sync);
-    let countMarkersToSync = markers.length;
-    let totalSynced = 0;
-    markers.forEach((marker) => {
-      marker.sync = true;
-      axios.post("https://hooks.zapier.com/hooks/catch/472009/09rj5z/?vinnialfonso@hotmail.com", {
-        latitude: marker.latitude,
-        longitude: marker.longitude,
-        annotation: marker.description,
-        datetime: marker.datetime
-      }).then((response) => {
-        if (response.status == 200){
-          totalSynced += 1;
-          if (totalSynced == countMarkersToSync){
-            this.setState({ syncing: false });
-          }
-        }
-      });
-    })
+  async isNetworkAvailable() {
+    const response = await NetInfo.fetch();
+    return response.isConnected;
+  }
+
+  setMarkers(){
     AsyncStorage.setItem('markers', JSON.stringify(this.state.markers));
     this.setState({
       markers: this.state.markers,
       countSync: this.state.markers.filter((m) => !m.sync).length
     })
+  }
+
+  async syncMarkers(){
+    let isConnected = await this.isNetworkAvailable();
+    if (isConnected){
+      this.setState({ syncing: true });
+      let markers = this.state.markers.filter((m) => !m.sync);
+      let countMarkersToSync = markers.length;
+      let totalSynced = 0, totalErrors = 0;
+      markers.forEach((marker) => {
+        axios.post("https://hooks.zapier.com/hooks/catch/472009/09rj5z/?vinnialfonso@hotmail.com", {
+          latitude: marker.latitude,
+          longitude: marker.longitude,
+          annotation: marker.description,
+          datetime: marker.datetime
+        }).then((response) => {
+          if (response.status == 200){
+            marker.sync = true;
+            totalSynced += 1;
+          } else {
+            marker.sync = false;
+            totalErrors += 1;
+          }
+          if ((totalErrors + totalSynced) == countMarkersToSync){
+            this.setState({ syncing: false });
+            this.setMarkers();
+            let strNoteSynced = totalSynced > 1 ? "anotações sincronizadas" : "anotação sincronizada";
+            let strNoteError = totalErrors > 1 ? "anotações não foram sincronizadas" : "anotação não foi sincronizada";
+            if (totalErrors > 0 && totalSynced == 0){
+              ToastAndroid.show(`${totalErrors} ${strNoteError}`, ToastAndroid.SHORT);
+            } else if (totalErrors > 0 && totalSynced > 0) {
+              ToastAndroid.show(`${totalSynced} ${strNoteSynced} e ${totalErrors} ${strNoteError}`, ToastAndroid.SHORT);
+            } else {
+              ToastAndroid.show(`${totalSynced} ${strNoteSynced}`, ToastAndroid.SHORT);
+            }
+          }
+        }, (errors) => {
+          this.setState({ syncing: false });
+        });
+      })
+    } else {
+      ToastAndroid.show(`Verifique sua conexão com a internet`, ToastAndroid.LONG);
+    }
   }
 
   render() {
@@ -123,16 +157,17 @@ export default class App extends Component {
             <Marker
               key={index}
               coordinate={marker.latlng}
-              title={marker.title}
-              description={marker.description}
               image={marker.sync ? require('../assets/images/gray-marker.png') : require('../assets/images/green-marker.png')}
             >
-              <Callout tooltip style={{ backgroundColor: "#FFFFFF", borderColor: "#e4e9ee", minWidth: 100, maxWidth: 400, paddingTop: 2, paddingBottom: 2, paddingRight: 5, paddingLeft: 5 }}>
+              <Callout tooltip style={{ backgroundColor: "#FFFFFF", borderColor: "#CCC", minWidth: 100, maxWidth: 400, paddingTop: 2, paddingBottom: 2, paddingRight: 5, paddingLeft: 5 }}>
                 <Text style={{ fontWeight: 'bold', alignSelf: 'center' }}>
                   Anotação
                 </Text>
                 <Text>
                   {marker.title}
+                </Text>
+                <Text>
+                  {marker.date}
                 </Text>
               </Callout>
             </Marker>
